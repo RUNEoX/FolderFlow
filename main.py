1b986b931413c28eb3ec0e1eb67c077dbe35921f
RUNEoX <qmyeyb07@gmail.com>
2025-04-18 11:27:31 +0300

    Update
    This time , the tool actually work

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from gui_interface import launch_gui
-from cli_interface import handle_cli
-
-if __name__ == "__main__":
-    if "--cli" in sys.argv:
-        handle_cli()  # Start in CLI mode if the --cli argument is passed
-    else:
-        launch_gui()  # Default: launch the GUI
